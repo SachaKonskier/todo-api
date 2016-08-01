5f46c62c53bcbaa6a80c1382e0a05e7270ef002f
Sacha Konskier <sacha.konskier@gmail.com>
2016-08-01 15:53:31 +0200

    Add ?completed filter

--- server.js
+++ server.js
@@ -14,9 +14,18 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -35,7 +44,7 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 
@@ -75,7 +84,7 @@ app.put('/todos/:id', function(req, res) {
 		return res.status(404).send();
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+	if (body.hasOwnProperty('completed') && !_.isBoolean(body.completed)) {
 		validAttributes.completed = body.completed;
 	} else if (body.hasOwnProperty('completed')) {
 		return res.status(400).send();
